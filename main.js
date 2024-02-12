c877a707bbe5a227d863ac7237a7a4bf6ea747a1
Samuel Jo <ctk.sam.jo@gmail.com>
2024-02-13 01:30:07 +0900

    second commit

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ let userInput = document.querySelector("#user-input");
 let playButton = document.querySelector("#play-button");
 let resultDisplay = document.querySelector(".result-display");
 let resetButton = document.querySelector("#reset-button");
+let chanceCount = document.querySelector(".chance-count");
+let chanceRemainder = 5;
+let gameOver = false;
 
 // playButton 함수 생성
 playButton = addEventListener("click", play);
@@ -17,12 +20,20 @@ function randomNumber() {
 //play 함수
 function play() {
   let userValue = userInput.value;
+  chanceRemainder--;
+  chanceCount.textContent = `남은 기회${chanceRemainder}번`;
   if (userValue < computerNumber) {
     resultDisplay.textContent = "좀 더 쓰세요";
   } else if (userValue > computerNumber) {
     resultDisplay.textContent = "좀 줄이세요";
   } else {
     resultDisplay.textContent = "어떻게 알았지?";
+  }
+  if (chanceRemainder < 1) {
+    gameOver = true;
+  }
+  if (gameOver == true) {
+    playButton.disabled = true;
   }
 }
 
