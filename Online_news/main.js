76ed729ee6cd62ac473d363ed5661af264e63032
Samuel Jo <ctk.sam.jo@gmail.com>
2024-02-22 00:17:49 +0900

    first commit

--- Online_news/main.js
+++ Online_news/main.js
@@ -1,13 +1,39 @@
 const API_KEY = "2f3f7acad3a64cf0bdbadc0863012489";
-let news = [];
+let newsList = [];
 const getLatestNews = async () => {
   const url = new URL(
     `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
   );
   const response = await fetch(url);
   const data = await response.json();
-  news = data.articles;
-  console.log("ddddd", news);
+  newsList = data.articles;
+  render();
+  console.log("ddddd", newsList);
+};
+
+const render = () => {
+  const newsHtml = newsList
+    .map(
+      (news) => `<section id="news-board">
+  <div class="row news">
+    <div class="col-lg-4">
+      <img
+        class="image-size"
+        src=${news.urlToImage}
+      />
+    </div>
+    <div class="col-lg-8">
+      <h1>${news.title}</h1>
+      <p>
+        ${news.description}
+      </p>
+      <div>${news.source.name} ${news.publishedAt}</div>
+    </div>
+  </div>
+</section>`
+    )
+    .join("");
+  document.getElementById("news-board").innerHTML = newsHtml;
 };
 
 getLatestNews();
--- main.js
+++ main.js
@@ -1,74 +1,39 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let taskInput = document.getElementById("input-box");
-  let addButton = document.getElementById("add-button");
-  let taskBoard = document.getElementById("task-board");
-  let tabs = document.querySelectorAll(".task-tabs div");
-  let taskList = [];
+const API_KEY = "2f3f7acad3a64cf0bdbadc0863012489";
+let newsList = [];
+const getLatestNews = async () => {
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+  render();
+  console.log("ddddd", newsList);
+};
 
-  addButton.addEventListener("click", addTask);
-  taskBoard.addEventListener("click", handleButtonClick);
+const render = () => {
+  const newsHtml = newsList
+    .map(
+      (news) => `<section id="news-board">
+  <div class="row news">
+    <div class="col-lg-4">
+      <img
+        class="image-size"
+        src=${news.urlToImage}
+      />
+    </div>
+    <div class="col-lg-8">
+      <h1>${news.title}</h1>
+      <p>
+        ${news.description}
+      </p>
+      <div>${news.source.name} ${news.publishedAt}</div>
+    </div>
+  </div>
+</section>`
+    )
+    .join("");
+  document.getElementById("news-board").innerHTML = newsHtml;
+};
 
-  for (let i = 1; i < tabs.length; i++) {
-    tabs[i].addEventListener("click", function (event) {
-      filter(event);
-    });
-  }
-
-  function addTask() {
-    let taskText = taskInput.value.trim();
-    if (taskText !== "") {
-      taskList.push({ text: taskText, checked: false });
-      render();
-      taskInput.value = ""; // Clear input field
-    } else {
-      alert("할 일을 타이핑 해주세요");
-    }
-  }
-
-  function handleButtonClick(event) {
-    if (event.target.tagName === "BUTTON") {
-      let action = event.target.textContent.toLowerCase();
-      let taskIndex = event.target.parentNode.parentNode.dataset.index;
-      if (action === "check") {
-        // Toggle the checked property
-        taskList[taskIndex].checked = !taskList[taskIndex].checked;
-        render();
-      } else if (action === "delete") {
-        // Implement delete functionality
-        taskList.splice(taskIndex, 1);
-        render();
-      }
-    }
-  }
-
-  function filter(event) {
-    let mode = event.target.id;
-    if (mode === "all") {
-      console.log(taskList);
-      render(taskList); // Render all tasks
-    } else if (mode === "onGoing") {
-      let filterList = taskList.filter((task) => !task.checked); // Filter unchecked tasks
-      render(filterList);
-    } else if (mode === "completed") {
-      let filterList = taskList.filter((task) => task.checked); // Filter checked tasks
-      render(filterList);
-    }
-  }
-
-  function render(tasks = taskList) {
-    let resultHTML = "";
-    tasks.forEach((task, index) => {
-      let taskClass = task.checked ? "task checked" : "task";
-      let taskText = task.checked ? `<s>${task.text}</s>` : task.text;
-      resultHTML += `
-        <div class="${taskClass}" data-index="${index}">
-          <div>${taskText}</div>
-          <div>
-            <button>Check</button>
-            <button>Delete</button>
-          </div>
-        </div>`;
-    });
-    taskBoard.innerHTML = resultHTML;
-  }
-});
+getLatestNews();
